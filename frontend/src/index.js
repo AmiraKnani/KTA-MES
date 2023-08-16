69e7c94e3e08ca2d71d0dcf2ab805f3a20948cbc
AmiraKnani <104366049+AmiraKnani@users.noreply.github.com>
2023-08-16 14:45:54 +0100

    version 16/08 axios

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 
+axios.defaults.baseURL = "http://192.168.2.249:3001/";
+
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+
+  "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
